8281831335a679f25308d137aa658ee793e03e54
artempelevin <artempelevin@list.ru>
2022-06-18 22:17:18 +0300

    Refactoring Back to the old style for an "Optional" hint

--- grabbers/plain_text.py
+++ grabbers/plain_text.py
@@ -1,15 +1,17 @@
+from typing import Optional, Union
+
 import requests
 
-from proxy.types import ProxyProvider, Proxy, ProvideProxyException, ProxyType
+from grabbers.types import ProxyProvider, Proxy, ProvideProxyException, ProxyType
 
 
 class PlainTextProxyProvider(ProxyProvider):
-    def __init__(self, url: str, proxy_type: ProxyType, delimiter: str | None = '\n') -> None:
+    def __init__(self, url: str, proxy_type: ProxyType, delimiter: Optional[str] = '\n') -> None:
         self._url = url
         self._proxy_type = proxy_type
         self._delimiter = delimiter
 
-    def provide_proxies(self, with_type: bool | None = True) -> list[Proxy] | list[str]:
+    def provide_proxies(self, with_type: Optional[bool] = True) -> Union[list[Proxy], list[str]]:
         try:
             request = requests.get(url=self._url)
             proxies = request.text.split(self._delimiter)
